6245c4d0b1499f1f153dddb01da8aa7f6f9cc14c
Ivana <29359501+horvana@users.noreply.github.com>
2023-09-17 13:38:49 +0200

    added contact details

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React from "react";
 
 export default function Footer() {
   return (
     <footer>
-      <h2>Contact Me</h2>
-      <p>Email: <a href="mailto:ivana.hrvtc@gmail.com">ivana.hrvtc@gmail.com</a></p>
+        <address>
+        <h2>Contact Me</h2>
+      <p>
+        <a href="mailto:ivana.hrvtc@gmail.com">Email</a>
+      </p>
+      <p>
+        <a
+          href="https://mega.nz/file/guVghIwY#M-W78MbP1Cjwty86ijxvu5odtik8kb40hA70aH_YBgA"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My CV
+        </a>
+      </p>
+      <p>
+        <a
+          href="https://www.linkedin.com/in/ivana-horvatic/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My LinkedIN
+        </a>
+      </p>
+        </address>
     </footer>
   );
 }
